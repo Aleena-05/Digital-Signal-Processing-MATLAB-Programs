b6578f0aed32c10eb7a621e4dbd9367ef8346f35
Aleena-05 <65156832+Aleena-05@users.noreply.github.com>
2021-01-09 12:27:21 +0530

    Add files via upload
    Linear convolution with inbuilt function in MATLAB

---
+++ lin_convolution_inbuilt.m
@@ -0,0 +1,6 @@
+x=input('Enter the first sequence to be convoluted:');
+h=input('Enter the second sequence to be convoluted:');
+
+f=conv(x,h);
+disp('The Linear convoluted sequence by inbuilt function is');
+disp(f);
